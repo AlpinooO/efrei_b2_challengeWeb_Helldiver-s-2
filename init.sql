8fdbd5aec1ad3def9a48c7829fbf13568382f2ae
AlpinooO <alpino.cdc@gmail.com>
2025-01-08 14:43:32 +0100

    Ajout UserController/model et modif SQL

--- init.sql
+++ init.sql
@@ -24,4 +24,6 @@ CREATE TABLE IF NOT EXISTS posts(
 	message varchar(255) NOT NULL,
 	parent int,
 	id_user int NOT NULL REFERENCES users
-)
+);
+
+INSERT INTO roles(titre) VALUES ('admin'), ('utilisateur');
---
+++ src/Controllers/UserController.php
@@ -0,0 +1,80 @@
+<?php
+namespace App\Controllers;
+
+use App\Controllers\CoreController;
+use App\Models\userModel;
+
+
+class UserController extends CoreController
+{
+    // Page "logout"
+    public function logout()
+    {
+        $this->isConnected();
+        session_unset(); // Supprime toutes les variables de session
+        session_destroy(); // Détruit la session
+        header('Location : /');
+        exit();
+    }
+    //page "register"
+    public function register()
+    {
+        $this->render('user/register');
+    }
+
+    // log l'utilisateur si les données correspondent
+    public function loginUser()
+    {
+        if (
+            (isset($_POST['email']) && !empty($_POST['email']))
+            && (isset($_POST['MDP']) && !empty($_POST['MDP']))
+        ) {
+            $userModel = new UserModel($_POST['email'], $_POST['MDP']); // verrifie si toutes les données existe
+            $userMDP = $userModel->login(); // cherche le MDP depuis l'email
+
+            if ($userMDP['MDP'] == $_POST['MDP']) { // commparre les MDP
+                $userID = $userModel->getUserId();
+                $_SESSION['userID'] = $userID['id_user']; // met dans la session l'id de l'utilisateur
+
+                $userRole = $userModel->getRole();
+                $_SESSION['userRole'] = $userRole['titre']; // met dans la session le role de l'utilisateur
+                header('Location : /'); // renvient sur home
+                exit();
+
+            } else {
+                $this->render('user/login');
+                echo '<script>alert("Mot de passe ou identifiant incorrect");</script>'; // envoie une  alert comme quoi le MDP est mauvais
+            }
+        }
+    }
+
+    // enregistre un utilisateur
+    public function registerUser()
+    {
+        if (
+            (isset($_POST['email']) && !empty($_POST['email']))
+            && (isset($_POST['MDP']) && !empty($_POST['MDP']))
+            && (isset($_POST['nom']) && !empty($_POST['nom']))
+            && (isset($_POST['prenom']) && !empty($_POST['prenom']))
+        ) { // verrification des données
+            $userModel = new UserModel($_POST['email'], $_POST['MDP'], $_POST['nom'], $_POST['prenom']);
+            if (!$userModel->getEmail()) { // verrifie si l'email existe déjà
+                echo 'a';
+                $userID = $userModel->register();
+                $_SESSION['userID'] = $userID['id_user']; // met dans la session l'id de l'utilisateur
+
+                $userRole = $userModel->getRole();
+                $_SESSION['userRole'] = $userRole['titre']; // met dans la session le role de l'utilisateur
+
+                header("Location : /"); // renvient sur home
+                exit();
+            } else {
+                $this->register(); // renvoie sur la page register
+                echo '<script>alert("l\'email existe déjà");</script>'; // envoie une alert comme quoi l'email existe déjà
+            }
+        } else {
+            $this->register(); // renvoie sur la page register
+            echo '<script>alert("donné incorecte ou manquante");</script>'; // envoie une alert comme quoi les données sont mauvaise
+        }
+    }
+}

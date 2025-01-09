9a6f7cbb8a17f19209d85c37b6f27e5963489037
toscy <123191534+Lajous-Harold@users.noreply.github.com>
2025-01-09 12:10:34 +0100

    [Fix] Integration page 404

--- templates/404.php
+++ templates/404.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/stylesheets/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+    <title>DiversHelper - 404 Page Not Found</title>
+</head>
+<body>
 <section class="container-404">
     <h1><i class="fa-solid fa-triangle-exclamation rouge"></i> Nouveau message reçu <i class="fa-solid fa-triangle-exclamation rouge"></i></h1>
     <div class="message-404">
@@ -25,4 +35,5 @@ echo $truncatedPath;
         <p>Nous sommes désolés mais la page que vous recherchez n'existe pas. Veuillez vérifier l'URL ou consulter notre <a href="/">accueil</a>.</p>
     </div>
 </section>
-    
+</body>
+</html>

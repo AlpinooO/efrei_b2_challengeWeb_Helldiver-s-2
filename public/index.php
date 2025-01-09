6b8c52035900514105d0b9148bd3860281ab0063
Pmaioranool <lucasmaiorano77@gmail.com>
2025-01-09 10:59:39 +0100

    changemen t de couleur mineur et [fix] de la signature

--- public/index.php
+++ public/index.php
@@ -17,8 +17,8 @@ $router->addRoute('GET', '/', MainController::class . '@home', 'home');
 $router->addRoute('GET', '/log', UserController::class . '@log', 'log');
 $router->addRoute('POST', '/log', MainController::class . '@log', 'logPost');
 $router->addRoute('GET', '/logout', UserController::class . '@logout', 'logout');
-$router->addRoute('GET', '/post', PostsController::class . '@post', 'post');
-$router->addRoute('POST', '/post', PostsController::class . '@poster', 'postPost');
+$router->addRoute('GET', '/forum', PostsController::class . '@post', 'post');
+$router->addRoute('POST', '/forum', PostsController::class . '@poster', 'postPost');
 $router->addRoute('GET', '/stratagem', MainController::class . '@stratagem', 'stratagem');
 $router->addRoute('GET', '/species', MainController::class . '@species', 'species');
 
